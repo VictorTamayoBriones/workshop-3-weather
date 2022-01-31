a69a4600230796c43665bd201eba2ed469a0495f
VictorTamayoBriones <VictorTamayoBriones@users.noreply.github.com>
2022-01-31 04:26:56 +0000

    deploy: dde5f171685c5b9646116ca5b787624237885f76

--- _snowpack/env.js
+++ _snowpack/env.js
@@ -1,5 +1,3 @@
-export const SNOWPACK_PUBLIC_API_KEY = "25745ce0da2954b896c845ab208d6851";
-export const SNOWPACK_PUBLIC_API_URL = "http://api.openweathermap.org/";
 export const API_URL = "http://api.openweathermap.org/";
 export const API_KEY = "25745ce0da2954b896c845ab208d6851";
 export const MODE = "production";
